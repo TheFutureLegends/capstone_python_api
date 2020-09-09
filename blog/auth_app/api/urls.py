fc8e905eb8ca7421f33b9a970678836c127b503a
leonguyen0202 <leonguyen0202@gmail.com>
2020-09-09 13:19:59 +0700

    Add authenticated urls comment inside auth_app

--- blog/auth_app/api/urls.py
+++ blog/auth_app/api/urls.py
@@ -5,3 +5,14 @@ urlpatterns = [
     path("", include('djoser.urls')),
     path("", include('djoser.urls.jwt'))
 ]
+
+# /auth/users/ -> register new user in auth_user table
+#### Params: username, email, password
+
+# /auth/jwt/create/ -> get new JWT for user
+#### Params: username, password
+#### Response: {access: <string>, refresh: <string>}
+
+# /auth/users/me/ -> get current authenticated user
+#### Headers: Bearer + access
+#### Response: {username: "", email: "", id: <int>} 
