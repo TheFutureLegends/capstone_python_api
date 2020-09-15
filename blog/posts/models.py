f87499df42b31bc687bf8dc26f90987c1884ced7
Minh <s3759797@rmit.edu.vn>
2020-09-16 05:11:11 +0700

    Attempt 6 to fix error

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.contrib.auth.models import User
 
-from blog.utils import unique_slug_generator
+from blog.blog.utils import unique_slug_generator
 
 # from users.models import Users
 
--- blog/showcases/models.py
+++ blog/showcases/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.signals import pre_save
-from blog.utils import unique_slug_generator
+from blog.blog.utils import unique_slug_generator
 
 # Create your models here.
 class Showcases(models.Model):
